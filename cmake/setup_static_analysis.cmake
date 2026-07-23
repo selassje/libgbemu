01fd1371c56fecc4d54c30121c876e16d68c8278
Przemyslaw  Koziol <przemkoz85@gmail.com>
2026-07-23 05:21:02 +0200

    msvc analysis

--- cmake/setup_static_analysis.cmake
+++ cmake/setup_static_analysis.cmake
@@ -8,3 +8,15 @@ function(setup_clang_tidy TARGET)
                                                 "${CLANG_TIDY_EXE}")
   endif()
 endfunction(setup_clang_tidy TARGET)
+
+function(setup_msvc_analysis TARGET)
+  if(ENABLE_MSVC_ANALYSIS)
+    set(RULESET_FILE ${CMAKE_SOURCE_DIR}/.vsAnalyze.ruleset)
+    get_filename_component(CXX_DIR ${CMAKE_CXX_COMPILER} DIRECTORY)
+    set(PLUGIN ${CXX_DIR}/EspXEngine.dll)
+    target_compile_definitions(${TARGET} PRIVATE CODE_ANALYSIS)
+    target_compile_options(
+      ${TARGET} PRIVATE /analyze /analyze:ruleset${RULESET_FILE}
+                        /analyze:plugin${PLUGIN} /analyze:external-)
+  endif()
+endfunction(setup_msvc_analysis)
