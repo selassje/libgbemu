3a95224242f87733758b257578bd780348676669
Przemyslaw Koziol <przemkoz85@gmail.com>
2026-07-27 01:35:33 +0200

    more

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -1,5 +1,7 @@
 module gbemu;
 
+import :regs;
+
 namespace {
 
 constexpr std::uint8_t TOTAL_SCANLINES = 154;
@@ -15,6 +17,109 @@ constexpr std::uint8_t LCD_ENABLE_BIT = 0x80;
 }
 
 namespace gbemu {
+
+void
+Ppu::Fifo::push(std::uint8_t pixel)
+{
+  m_buffer.at((m_head + m_size) % m_buffer.size()) = pixel;
+  ++m_size;
+}
+
+std::uint8_t
+Ppu::Fifo::pop()
+{
+  const auto pixel = m_buffer.at(m_head);
+  m_head = (m_head + 1) % m_buffer.size();
+  --m_size;
+  return pixel;
+}
+
+void
+Ppu::Fetcher::runNextTCycle()
+{
+  const auto elapsedDots = m_ppu.get().m_dot - m_lastDotStateChange;
+  const auto currentState = m_mState;
+  constexpr std::uint16_t tileDataBlock0 = 0x8000; // NOLINT(readability-magic-numbers)
+  constexpr std::uint16_t tileDataBlock1 = 0x8800; // NOLINT(readability-magic-numbers) ]
+  constexpr std::uint16_t tileDataBlock2 = 0x9000; // NOLINT(readability-magic-numbers) ]
+
+  const auto pushTileRowToFifo = [this]() {
+    if (m_ppu.get().m_bgWndFifo.size() >= 8) { // NOLINT(readability-magic-numbers)
+      return false;
+    }
+    for (unsigned bit = 8; bit-- > 0;) { // NOLINT(readability-magic-numbers)
+      const auto colorIndex = static_cast<std::uint8_t>(
+        (((static_cast<unsigned>(m_tileDataHigh) >> bit) & 1U) << 1U)
+        | ((static_cast<unsigned>(m_tileDataLow) >> bit) & 1U));
+      m_ppu.get().m_bgWndFifo.push(colorIndex);
+    }
+    return true;
+  };
+
+  switch (m_mState) {
+    case State::ReadTile:
+      if (elapsedDots >= 1) {
+        constexpr std::uint16_t tileMapBaseAddress =
+          0x9800; // NOLINT(readability-magic-numbers)
+        const std::uint8_t tileX =
+          (m_X + (m_mmu.get().readByte(regs::SCX) / 8)) % 32;
+        const std::uint8_t tileY =
+          (m_ppu.get().m_scanline + m_mmu.get().readByte(regs::SCY)) % 256;
+        const auto tileMapAddress = static_cast<std::uint16_t>(
+          tileMapBaseAddress + ((tileY / 8) * 32) + tileX);
+        m_mTileIndex = m_mmu.get().readByte(tileMapAddress);
+        m_mState = State::ReadTileDataLow;
+      }
+      break;
+    case State::ReadTileDataLow:
+    case State::ReadTileDataHigh:
+      if (elapsedDots >= 1) {
+        const bool isHighByte = (m_mState == State::ReadTileDataHigh);
+        const auto rowOffset = static_cast<std::uint16_t>(
+          (((m_ppu.get().m_scanline + m_mmu.get().readByte(regs::SCY)) % 8)
+           * 2)
+          + (isHighByte ? 1 : 0));
+        std::uint8_t tileByte{};
+        if (m_mTileIndex >= 128) {
+          const auto tileOffset = static_cast<std::uint16_t>(
+            ((m_mTileIndex - 128) * 16) + rowOffset);
+          tileByte = m_mmu.get().readByte(tileDataBlock1 + tileOffset);
+        } else {
+          const auto tileOffset = static_cast<std::uint16_t>(
+            (m_mTileIndex * 16) + rowOffset);
+          const auto lcdc = m_mmu.get().readByte(regs::LCDC);
+          if ( (lcdc & 0x10U) != 0) { // NOLINT(readability-magic-numbers)
+            tileByte = m_mmu.get().readByte(tileDataBlock0 + tileOffset);
+          } else {
+            tileByte = m_mmu.get().readByte(tileDataBlock2 + tileOffset);
+          }
+        }
+        if (isHighByte) {
+          m_tileDataHigh = tileByte;
+          m_mState = State::Sleep;
+          pushTileRowToFifo();
+        } else {
+          m_tileDataLow = tileByte;
+          m_mState = State::ReadTileDataHigh;
+        }
+      }
+      break;
+    case State::Sleep:
+      if (elapsedDots >= 1) {
+        m_mState = State::PushToFifo;
+      }
+      break;
+    case State::PushToFifo:
+      if (pushTileRowToFifo()) {
+        m_mState = State::ReadTile;
+        ++m_X;
+      }
+      break;
+  }
+  if (currentState != m_mState) {
+    m_lastDotStateChange = m_ppu.get().m_dot;
+  }
+};
 
 void
 Ppu::runNextTCycle()
@@ -48,6 +153,7 @@ Ppu::runNextTCycle()
 void
 Ppu::incrementDot()
 {
+
   ++m_dot;
   if (m_dot >= DOTS_PER_SCANLINE) {
     m_dot = 0;
@@ -95,9 +201,9 @@ Ppu::handleOAMSearch() {
 };
 
 void
-Ppu::handlePixelTransfer() {
-  // Pixel Transfer logic can be implemented here
-
+Ppu::handlePixelTransfer()
+{
+  m_fetcher.runNextTCycle();
 };
 
 };
--- src/ppu.cppm
+++ src/ppu.cppm
@@ -1,3 +1,5 @@
+module;
+#include <functional>
 export module gbemu:ppu;
 
 import :mmu;
@@ -24,14 +26,61 @@ private:
     PixelTransfer = 3,
   };
 
+  class Fifo
+  {
+  public:
+    void push(std::uint8_t pixel);
+    std::uint8_t pop();
+    [[nodiscard]] bool empty() const { return m_size == 0; }
+    [[nodiscard]] std::size_t size() const { return m_size; }
+
+  private:
+    std::array<std::uint8_t, 16> m_buffer{};
+    std::size_t m_head{ 0 };
+    std::size_t m_size{ 0 };
+  };
+
+  class Fetcher
+  {
+  public:
+    Fetcher(Mmu& mmu, Ppu& ppu)
+      : m_mmu(mmu)
+      , m_ppu(ppu)
+    {
+    }
+
+    void runNextTCycle();
+
+  private:
+    enum class State : std::uint8_t
+    {
+      ReadTile,
+      ReadTileDataLow,
+      ReadTileDataHigh,
+      Sleep,
+      PushToFifo,
+    };
+
+    std::reference_wrapper<Mmu> m_mmu;
+    std::reference_wrapper<Ppu> m_ppu;
+    State m_mState{ State::ReadTile };
+    std::uint8_t m_X{ 0 };
+    std::uint8_t m_mTileIndex{ 0 };
+    std::uint16_t m_lastDotStateChange{ 0 };
+    std::uint8_t m_tileDataLow{};
+    std::uint8_t m_tileDataHigh{};
+  };
+
   std::reference_wrapper<Mmu> m_mmu;
   std::uint8_t m_scanline{ 0 };
   std::uint16_t m_dot{ 0 };
   Mode m_mode{ Mode::OAMSearch };
   std::uint8_t m_nextPixelXToRender{ 0 };
+  Fifo m_bgWndFifo{};
+  Fifo m_objFifo{};
+  Fetcher m_fetcher{ m_mmu, *this };
 
   void incrementDot();
-
   void handleHBlank();
   void handleVBlank();
   void handleOAMSearch();
